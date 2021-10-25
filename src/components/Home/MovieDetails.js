243204ce6728e73a6da54440409b826ae19807c0
Imran-2021 <mdimranulhaque.8dec20@gmail.com>
2021-10-25 21:15:38 +0600

    details page done

--- src/components/Home/MovieDetails.js
+++ src/components/Home/MovieDetails.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
+import { Link } from 'react-router-dom';
 
 const MovieDetails = () => {
     const {idd}=useParams()
-    console.log(idd);
+    const [movie,setMovie]=useState([])
+    useEffect(()=>{
+        const url= "https://api.tvmaze.com/search/shows?q=all"
+        fetch(url)
+        .then(res=>res.json())
+        .then(data=>{
+            const movie = data.filter(x=>x?.show?.id==idd)
+            setMovie(movie)
+        })
+    },[])
     return (
         <div>
-            <p>details of the id :{idd}</p>
+            <div>
+                {
+                  movie.map(mv=>{
+                      return (
+                          <>
+                          <img  src={mv.show.image.medium} alt="" />
+                          <h2>{mv.show.name}</h2>
+                          <h6>{mv.show.summary}</h6>
+                          <button className="btn btn-primary w-50"><Link className="text-link" to={`/movieDetails/${idd}`}>Buy Ticket Now</Link></button>
+                          </>
+                      )
+                  })
+                }
+            </div>
         </div>
     );
 };
